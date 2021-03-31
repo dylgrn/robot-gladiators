d094df3660b5bb5c3cb7bc9506a7c142dfd57607
dylan green <dylgrn.github.io>
2021-03-30 22:59:22 -0500

    for loop added

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,14 +1,22 @@
+// Game states
+// "WIN" - Player robot has defeated all enemy-robots
+// * Fight all enemy-robots
+// * Defeat each enemy-robot
+// "LOSE" - Player robot's health is zero or less
+
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 14;
 
-var enemyName = "Roberto";
+var enemyNames = ["Roberto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
 var playerMoney = 10;
 
-var fight = function() {
+
+
+var fight = function(enemyName) {
 // Alert players that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
 // prompt fight
@@ -52,14 +60,15 @@ if (promptFight === "fight" || promptFight === "FIGHT") {
       playerMoney = playerMoney - 2;
     }
     // if no (false), ask question again by running fight() again
-    else {
-      fight();
+     else {
+     // fight();
     }
-  }  
     window.alert(playerName + " has chosen to skip the fight!");
-  } else {
+    } else { 
     window.alert("You need to choose a valid option. Try again!");
-  }
-};
+    }
+}
 
-fight();
+for (var i = 0; i < enemyNames.length; i++) {
+  fight(enemyNames[i]);
+}
